946ca2294ec20420d523d1119f018b6beaf59a50
zhangshiyao1995 <zhangshiyao1995@users.noreply.github.com>
2018-02-20 19:44:47 -0500

    lab4Augular

---
+++ src/app/books/books.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {Book} from '../books/book';
+import {BOOKS} from '../books/mock-books-service'
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css']
+})
+export class BooksComponent implements OnInit {
+  
+
+  books = BOOKS;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/books/mock-books-service.ts
@@ -0,0 +1,6 @@
+import {Book} from './book';
+
+export const BOOKS: Book[]=[
+    {id: 1, title: 'Ready Player One', author: 'Ernest Cline'},
+    {id: 2, title: 'Catch 22', author: 'Joseph Heller'}
+];
